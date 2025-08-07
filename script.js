317a95a8d3647ebaf4cf104e3f32af854df0c0ed
sleepywndud <w2n2d2@gmail.com>
2025-08-08 09:35:16 +1200

    08.08.2025 Backup

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const navigation = document.querySelector('.navbar'); 
 window.addEventListener('scroll', () => { 
-    if (window.scrollY > 100) { 
+    if (window.scrollY > 500) { 
         navigation.classList.add('scrolled'); 
     } 
     else { 
